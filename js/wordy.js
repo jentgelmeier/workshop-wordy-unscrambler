2eda527d121e61c6c5523929abb1a9029e38b070
Jason Entgelmeier <jasonentgelmeier@Emilys-MacBook-Pro.local>
2023-10-31 13:52:34 -0500

    completed exercise

--- js/wordy.js
+++ js/wordy.js
@@ -1,22 +1,54 @@
+import deePool from "./external/deepool";
+
 export default {
 	loadWords,
-	findWords
+	findWords,
 };
 
+var dict = new MinimalWordGraph();
 
 // ****************************
 
 function loadWords(wordList) {
-	// TODO: implement a data structure for the array
-	// `wordList` parameter; return the number
-	// of entries inserted into the data structure
-	return 0;
+	if (dict.size() > 0) {
+		dict = new MininalWordGraph();
+	}
+
+	for (let word of wordList) {
+		dict.add(word);
+	}
+
+	dict.makeImmutable();
+
+	return dict.size();
 }
 
-function findWords(input) {
-	// TODO: implement unscrambling/searching logic
-	// for a string of uppercase letters in the
-	// `input` parameter; return the array of
-	// matching words
-	return [];
+function countLetters(str) {
+	var counts = [];
+	for (let i = 0; i < str.length; i++) {
+		counts[str[i]] = (counts[str[i]] || 0) + 1;
+	}
+	return counts;
+}
+
+function findWords(input, prefix = "", node = dict) {
+	var words = dict.containsOnly(
+		Array.isArray(input)
+			? input
+			: typeof input == "string"
+			? input.split("")
+			: []
+	);
+
+	words = [...new Set(words)];
+
+	return words.filter(function removeWords(word) {
+		var wordLetterCounts = countLetters(word);
+		for (let [letter, count] of Object.entries(wordLetterCounts)) {
+			if (!inputCounts[letter] || count > inputCounts[letter]) {
+				return false;
+			}
+		}
+		return true;
+	});
 }
